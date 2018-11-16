c6274ab9c2a89e7aa222e9ecbfa723b66041e774
Nick Liu <nl2523@columbia.edu>
2018-11-16 01:58:40 -0500

    integrated twilio client from TestFolder + removed TestFolder

--- automation/automation.py
+++ automation/automation.py
@@ -2,9 +2,14 @@ from automation import logger
 
 
 class Automation():
-    def __init__(self, config):
+    def __init__(self, config, twilioClient):
         self.config = config
         self.logger = logger
+        self.twilioClient = twilioClient
 
     def run(self):
-        self.logger.info("im running yo")
+        self.logger.info("running")
+        self.logger.info("Attempting to send SMS to phone num: +17342728388")
+        self.twilioClient.sendSMS("+17342728388")
+        self.logger.info("Finished sending attempt")
+        
