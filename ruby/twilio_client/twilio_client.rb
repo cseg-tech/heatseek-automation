60554b1008c80bab3ad31d84be37471ed4893bc4
Nick Liu <nl2523@columbia.edu>
2018-12-03 21:05:10 -0500

    added error checking + handling

--- ruby/twilio_client/twilio_client.rb
+++ ruby/twilio_client/twilio_client.rb
@@ -1,20 +1,27 @@
 require 'twilio-ruby'
+require 'logger'
 
 class TwilioClient
     def initialize(config)
         @config = config
+        @logger = Logger.new(STDOUT)
         account_sid = config['API_KEYS']['TWILIO_ACCOUNT_SID']
         auth_token = config['API_KEYS']['TWILIO_AUTH_TOKEN']
         @client = Twilio::REST::Client.new(account_sid, auth_token)
         @from_number = config['TWILIO']['FROM_PHONE_NUMBER']
         @message = config['TWILIO']['SMS_MESSAGE']
     end
+
     def sendSMS(phone_number)
+        begin
         @client.messages.create(
             to: phone_number,
             from: @from_number,
             body: @message
         )
+        rescue
+            @logger.warn("Message not sent, user said to STOP")
+        end
     end
 
 end
